4f132bfe18d235b46e08aa9cc1ab855a8b7bdd8b
jmulford-bandwidth <jmulford@bandwidth.com>
2020-12-08 13:17:56 -0500

    DX-1669 tag

---
+++ src/Voice/Bxml/Tag.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Tag.php
+ *
+ * Implementation of the BXML Tag verb
+ *
+ *  * @copyright Bandwidth INC
+ */
+  
+namespace BandwidthLib\Voice\Bxml;
+
+require_once "Verb.php";
+
+class Tag extends Verb {
+
+    /**
+     * Constructor for Tag
+     *
+     * @param string $tag The value to set the call tag to
+     */
+    public function __construct($tag) {
+        $this->tag = $tag;
+    }
+
+    public function toBxml($doc) {
+        $element = $doc->createElement("Tag", $this->tag);
+
+        return $element;
+    }
+}
