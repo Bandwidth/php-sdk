62f13c39520618ce65e733757e7dd4ac40eb764e
Audrey Copeland <acopeland@bandwidth.com>
2021-07-23 15:51:36 -0400

    Add documentation and fix tests

--- src/WebRtc/Utils/WebRtcTransfer.php
+++ src/WebRtc/Utils/WebRtcTransfer.php
@@ -11,10 +11,22 @@ namespace BandwidthLib\WebRtc\Utils;
 
 class WebRtcTransfer {
     public static function generateBxml($deviceToken, $voiceCallId, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
+        /**
+        * Returns BXML string with WebRTC a device token to perform a SIP transfer
+        * @param    string  $deviceToken  The device token
+        * @param    array   $voiceCallId  The Bandwidth Voice Call Id
+        * @return   string  $sipUri The SIP URI to transfer the call to 
+        */
         return '<?xml version="1.0" encoding="UTF-8"?><Response>' . WebRtcTransfer::generateTransferBxmlVerb($deviceToken, $voiceCallId, $sipUri) . '</Response>';
     }
 
     public static function generateTransferBxmlVerb($deviceToken, $voiceCallId, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
+        /**
+        * Returns the Transfer verb to perform the SIP transfer
+        * @param    string  $deviceToken  The device token
+        * @param    array   $voiceCallId  The Bandwidth Voice Call Id
+        * @return   string  $sipUri The SIP URI to transfer the call to 
+        */
         $formattedCallId = substr(str_replace("-", "", $voiceCallId), 1);
         return '<Transfer><SipUri uui="' . $formattedCallId . ';encoding=base64' . $deviceToken . ';encoding=jwt">' . $sipUri . '</SipUri></Transfer>'; 
     }
