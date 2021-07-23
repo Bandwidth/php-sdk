664623210dc7bfc7e66e4c5085d4f476d2d30584
Audrey Copeland <acopeland@bandwidth.com>
2021-07-23 09:16:07 -0400

    EDGE-630 Add voiceCallId to generateTransferBxmlVerb for debugging

--- src/WebRtc/Utils/WebRtcTransfer.php
+++ src/WebRtc/Utils/WebRtcTransfer.php
@@ -10,11 +10,11 @@
 namespace BandwidthLib\WebRtc\Utils;
 
 class WebRtcTransfer {
-    public static function generateBxml($deviceToken, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
-        return '<?xml version="1.0" encoding="UTF-8"?><Response>' . WebRtcTransfer::generateTransferBxmlVerb($deviceToken, $sipUri) . '</Response>';
+    public static function generateBxml($deviceToken, $voiceCallId, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
+        return '<?xml version="1.0" encoding="UTF-8"?><Response>' . WebRtcTransfer::generateTransferBxmlVerb($deviceToken, $voiceCallId, $sipUri) . '</Response>';
     }
 
-    public static function generateTransferBxmlVerb($deviceToken, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
-        return '<Transfer><SipUri uui="' . $deviceToken . ';encoding=jwt">' . $sipUri . '</SipUri></Transfer>'; 
+    public static function generateTransferBxmlVerb($deviceToken, $voiceCallId, $sipUri = "sip:sipx.webrtc.bandwidth.com:5060") {
+        return '<Transfer><SipUri uui="' . $voiceCallId . ';encoding=base64' . $deviceToken . ';encoding=jwt">' . $sipUri . '</SipUri></Transfer>'; 
     }
 }
