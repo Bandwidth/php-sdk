14659410a7f5ac9549bc2a1418bb81c882efa764
ckoegel <ckoegel1006@gmail.com>
2025-11-21 10:57:38 -0500

    tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -197,7 +197,9 @@ final class ApiTest extends TestCase
         $body = new BandwidthLib\PhoneNumberLookup\Models\CreateAsyncBulkRequest();
         $body->phoneNumbers = [getenv("USER_NUMBER")];
         $createResponse = $this->bandwidthClient->getPhoneNumberLookup()->getClient()->createAsyncBulkLookupRequest(getenv("BW_ACCOUNT_ID"), $body);
+        $this->assertInstanceOf(BandwidthLib\PhoneNumberLookup\Models\CreateAsyncBulkResponse::class, $createResponse->getResult()->data);
         $this->assertTrue(strlen($createResponse->getResult()->data->requestId) > 0);
+        $this->assertTrue(strlen($createResponse->getResult()->data->status) > 0);
 
         // $requestId = $createResponse->getResult()->requestId;
         // $getResponse = $this->bandwidthClient->getPhoneNumberLookup()->getClient()->getLookupRequestStatus(getenv("BW_ACCOUNT_ID"), $requestId);
